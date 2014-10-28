a0716a0c3553c21ab4408ccd5bc616ef9ce3da40
Abhinav <abhinav.iims@gmail.com>
2014-10-28 12:47:39 +0000

    Added copyright to mobile library

--- AnticipatoryMiddleware/src/org/ubhave/anticipatorymiddleware/event/DataCollectionService.java
+++ AnticipatoryMiddleware/src/org/ubhave/anticipatorymiddleware/event/DataCollectionService.java
@@ -1,3 +1,35 @@
+/*******************************************************************************
+ *
+ * FutureWare Middleware
+ *
+ * Copyright (c) ${2014}, University of Birmingham
+ * Abhinav Mehrotra, a.mehrotra@cs.bham.ac.uk
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Birmingham 
+ *       nor the names of its contributors may be used to endorse or
+ *       promote products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE ABOVE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************/
 package org.ubhave.anticipatorymiddleware.event;
 
 import java.util.ArrayList;
@@ -192,8 +224,6 @@ public class DataCollectionService extends IntentService {
 
 		if(classified_data.length()>1)
 		{
-			/**  Update latest context in SharedPreferences  **/
-			setLatestDataInSharedPreferences(data, classified_data);
 
 			if(data.getSensorType() == Constants.SENSOR_TYPE_APPLICATION
 					|| data.getSensorType() == Constants.SENSOR_TYPE_CALL_LOGS
@@ -201,6 +231,10 @@ public class DataCollectionService extends IntentService {
 			{
 
 				JSONArray classified_data_array = new JSONArray(classified_data);
+
+				/**  Update latest context in SharedPreferences  **/
+				setLatestDataInSharedPreferences(data, classified_data_array.getString(0));
+				
 				for(int i=0; i<classified_data_array.length(); i++)
 				{
 					JSONObject jsonObject = new JSONObject();
@@ -211,6 +245,9 @@ public class DataCollectionService extends IntentService {
 			}
 			else
 			{
+				/**  Update latest context in SharedPreferences  **/
+				setLatestDataInSharedPreferences(data, classified_data);
+				
 				JSONObject jsonObject = new JSONObject();
 				jsonObject.put("TIME", Time.getCurrentTime().toString());
 				jsonObject.put("STATE", classified_data);
--- AnticipatoryMiddleware/src/org/ubhave/anticipatorymiddleware/storage/SQLiteUtils.java
+++ AnticipatoryMiddleware/src/org/ubhave/anticipatorymiddleware/storage/SQLiteUtils.java
@@ -1,9 +1,41 @@
+/*******************************************************************************
+ *
+ * FutureWare Middleware
+ *
+ * Copyright (c) ${2014}, University of Birmingham
+ * Abhinav Mehrotra, a.mehrotra@cs.bham.ac.uk
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Birmingham 
+ *       nor the names of its contributors may be used to endorse or
+ *       promote products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE ABOVE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************/
 package org.ubhave.anticipatorymiddleware.storage;
 
 public class SQLiteUtils {
 	
 	
-	private final String DB_NAME = "FUTUREWARE_SQLite_V9.DB";
+	private final String DB_NAME = "FUTUREWARE_SQLite_V11.DB";
 	private String TABLE_STACKED_DATA = "TABLE_STACKED_DATA_";
 	private String TABLE_LABELLED_DATA = "TABLE_LABELLED_DATA_";
 	private String TABLE_MODEL_DATA = "TABLE_MODEL_DATA_";
